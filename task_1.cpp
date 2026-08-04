5124835c5bb1766500be5e5c2b17b29503d47d37
kHa-N-byte <24-58568-2@student.aiub.edu>
2026-08-04 14:28:53 +0600

    Refactor task_1.cpp for improved readability and structure

--- task_1.cpp
+++ task_1.cpp
@@ -1,243 +1,422 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cctype>
-
+#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
+using namespace std;
+int lineNumber = 0;
+char* idArray[100];
+int idCount = 0;
+char* opArray[100];
+int opCount = 0;
+char* libArray[100];
+int libCount = 0;
+char* invalidVarArray[100];
+int invalidVarCount = 0;
+bool semicolonMissing = false;
 
-int main()
+// Returns 'true' if the character is a DELIMITER.
+bool isDelimiter(char ch)
 {
-    ifstream fin("program.cpp");
+	if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
+		ch == '/' || ch == ',' || ch == ';' || ch == '>' ||
+		ch == '<' || ch == '=' || ch == '(' || ch == ')' ||
+		ch == '[' || ch == ']' || ch == '{' || ch == '}' ||
+		ch=='\t'||ch=='\n'||ch=='&'||ch=='^'||ch=='?'||
+		ch=='|' ||ch==':'||ch=='.'|| ch=='"'||ch=='#'||
+		ch=='!' )
+		return (true);
+	return (false);
+}
 
-    if(!fin)
+// Returns 'true' if the character is an OPERATOR.
+bool isOperator(char ch)
+{
+	if (ch == '+' || ch == '-' || ch == '*' ||
+		ch == '/' || ch == '>' || ch == '<' ||
+		ch == '%' || ch == '='||ch=='-='||ch=='+='||
+		ch =='--'||ch =='++'||ch=='-+'||
+		ch=='/*'||ch=='*='||ch=='||'||
+		ch=='&&'||ch=='=='||ch=='<='||
+		ch=='=>'||ch=='!'||ch=='!='||
+		ch=='/=')
+		return (true);
+	return (false);
+}
+
+// Returns 'true' if the string is a VALID IDENTIFIER.
+bool validIdentifier(char* str)
+{
+	if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
+		str[0] == '3' || str[0] == '4' || str[0] == '5' ||
+		str[0] == '6' || str[0] == '7' || str[0] == '8' ||
+		str[0] == '9' || isDelimiter(str[0]) == true)
+		return (false);
+	return (true);
+}
+//bool isPreprocessor(char* substr){
+    //return true|false;
+//}
+
+
+// Returns 'true' if the string is a KEYWORD.
+bool isKeyword(char* str)
+{
+	if (!strcmp(str, "if") || !strcmp(str, "else") ||
+		!strcmp(str, "while") || !strcmp(str, "do") ||
+		!strcmp(str, "break") || !strcmp(str, "for") ||
+		!strcmp(str, "continue") || !strcmp(str, "int") ||
+		!strcmp(str, "double") || !strcmp(str, "float") ||
+		!strcmp(str, "return") || !strcmp(str, "char") ||
+		!strcmp(str, "case") || !strcmp(str, "char") ||
+		!strcmp(str, "sizeof") || !strcmp(str, "long") ||
+		!strcmp(str, "short") || !strcmp(str, "typedef") ||
+		!strcmp(str, "switch") || !strcmp(str, "unsigned") ||
+		!strcmp(str, "void") || !strcmp(str, "static") ||
+		!strcmp(str, "struct") || !strcmp(str, "goto") ||
+		!strcmp(str, "using") || !strcmp(str, "namespace") ||
+		!strcmp(str, "cout") || !strcmp(str, "cin") ||
+		!strcmp(str, "endl") || !strcmp(str, "return") ||
+		!strcmp(str, "main") || !strcmp(str, "include") ||
+		!strcmp(str, "for") || !strcmp(str, "int") || !strcmp(str, "std"))
+		return (true);
+	return (false);
+}
+
+// Returns 'true' if the string is an INTEGER.
+bool isInteger(char* str)
+{
+	int i, len = strlen(str);
+
+	if (len == 0)
+		return (false);
+	for (i = 0; i < len; i++) {
+		if ((str[i] != '0' && str[i] != '1' && str[i] != '2'
+			&& str[i] != '3' && str[i] != '4' && str[i] != '5'
+			&& str[i] != '6' && str[i] != '7' && str[i] != '8'
+			&& str[i] != '9') ||(str[i] == '-' && i >  0))
+			return (false);
+	}
+	return (true);
+}
+
+// Returns 'true' if the string is a REAL NUMBER.
+bool isRealNumber(char* str)
+{
+	int i, len = strlen(str);
+	bool hasDecimal = false;
+
+	if (len == 0)
+		return (false);
+	for (i = 0; i < len; i++) {
+		if (str[i] != '0' && str[i] != '1' && str[i] != '2'
+			&& str[i] != '3' && str[i] != '4' && str[i] != '5'
+			&& str[i] != '6' && str[i] != '7' && str[i] != '8'
+			&& str[i] != '9' && str[i] != '.' ||
+			(str[i] == '-' && i > 0))
+			return (false);
+		if (str[i] == '.')
+			hasDecimal = true;
+	}
+	return (hasDecimal);
+}
+
+// Extracts the SUBSTRING.
+char* subString(char* str, int left, int right)
+{
+	int i;
+	char* subStr = (char*)malloc(
+				sizeof(char) * (right - left + 2));
+
+	for (i = left; i <= right; i++)
+		subStr[i - left] = str[i];
+	subStr[right - left + 1] = '\0';
+	return (subStr);
+}
+// Function to add unique variable to array
+void addUniqueVariable(char* str) {
+	// Check if already exists
+	for (int i = 0; i < idCount; i++) {
+		if (strcmp(idArray[i], str) == 0)
+			return;
+	}
+
+	idArray[idCount] = (char*)malloc(strlen(str) + 1);
+	strcpy(idArray[idCount], str);
+	idCount++;
+}
+// Function to add unique operator to array
+void addUniqueOperator(char ch) {
+	char op[2] = {ch, '\0'};
+	for (int i = 0; i < opCount; i++) {
+		if (strcmp(opArray[i], op) == 0)
+			return;
+	}
+	opArray[opCount] = (char*)malloc(2);
+	strcpy(opArray[opCount], op);
+	opCount++;
+}
+// Function to add unique library
+void addUniqueLibrary(char* str) {
+	for (int i = 0; i < libCount; i++) {
+		if (strcmp(libArray[i], str) == 0)
+			return;
+	}
+	libArray[libCount] = (char*)malloc(strlen(str) + 1);
+	strcpy(libArray[libCount], str);
+	libCount++;
+}
+void addInvalidVariable(char* str) {
+	for (int i = 0; i < invalidVarCount; i++) {
+		if (strcmp(invalidVarArray[i], str) == 0)
+			return;
+	}
+	invalidVarArray[invalidVarCount] = (char*)malloc(strlen(str) + 1);
+	strcpy(invalidVarArray[invalidVarCount], str);
+	invalidVarCount++;
+}
+// Function to check valid expression
+bool checkValidExpression(char* tokens[], int tokenCount) {
+	if (tokenCount < 3) return false;
+
+	// Check if token at index 1 is '='
+	if (strcmp(tokens[1], "=") != 0) return false;
+
+	// Check token at index 0 must be a valid identifier
+	if (!validIdentifier(tokens[0]) || isKeyword(tokens[0])) return false;
+
+	// Check even indexes (2,4,6,...) must be variables or integers
+	// Check odd indexes (3,5,7,...) must be valid operators
+	for (int i = 2; i < tokenCount; i++) {
+		if (i % 2 == 0) {
+			// Even index: must be variable or integer
+			char* temp = tokens[i];
+			// Remove semicolon if present
+			int len = strlen(temp);
+			if (temp[len-1] == ';') {
+				temp[len-1] = '\0';
+			}
+			if (!validIdentifier(temp) && !isInteger(temp)) {
+				return false;
+			}
+		} else {
+			// Odd index: must be operator
+			if (!isOperator(tokens[i][0])) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+// Parsing the input STRING.
+void parse(char* str)
+{
+	int left = 0, right = 0;
+	int len = strlen(str);
+	char* tokens[100];
+	int tokenCount = 0;
+	int start = 0;
+
+    // Skip leading spaces and tabs
+    while(start < len && (str[start] == ' ' || str[start] == '\t'))
     {
-        cout<<"Cannot open file";
-        return 0;
+      start++;
     }
 
-    string line;
-    int lineNo=0;
+    if (len > 0 &&
+      str[len-1] != ';' &&
+      str[start] != '#' &&
+      str[start] != '/' &&
+      str[start] != '{' &&
+      str[start] != '}' &&
+      strncmp(str + start, "for", 3) != 0 &&
+      strncmp(str + start, "if", 2) != 0 &&
+      strncmp(str + start, "while", 5) != 0 &&
+      strncmp(str + start, "int main()", 10) != 0 )
 
-    string variables[100];
-    int varCount=0;
-
-    char operators[100];
-    int opCount=0;
-
-    int libCount=0;
+      {
+        printf("\";\" missing in line - %d\n", lineNumber);
+      }
 
 
-    while(getline(fin,line))
+	for(int i=0; i<len-1; i++)
     {
-        lineNo++;
-
-
-        // Library detection
-        if(line.find("#include") == 0)
-        {
-            int l=line.find('<');
-            int r=line.find('>');
-
-            if(l!=-1 && r!=-1)
-            {
-                libCount++;
-                cout<<"Library-"<<libCount<<": "
-                    <<line.substr(l+1,r-l-1)<<endl;
-            }
-        }
-
-
-        // Comment detection
-        int c=line.find("//");
-
-        if(c!=-1)
-        {
-            cout<<"Comment in line - "<<lineNo<<endl;
-        }
-
-
-        // Missing semicolon
-        string temp=line;
-
-        while(temp.length()>0 &&
-             (temp[temp.length()-1]==' ' || temp[temp.length()-1]=='\t'))
-        {
-            temp.pop_back();
-        }
-
-
-        if(temp.length()>0 &&
-           temp[0]!='#' &&
-           temp[temp.length()-1]!=';' &&
-           temp[temp.length()-1]!='{' &&
-           temp[temp.length()-1]!='}')
-        {
-            cout<<"';' missing in line - "<<lineNo<<endl;
-        }
-
-
-        // Missing parenthesis
-        int open=0, close=0;
-
-        for(int i=0;i<line.length();i++)
-        {
-            if(line[i]=='(')
-                open++;
-
-            if(line[i]==')')
-                close++;
-        }
-
-        if(open!=close)
-        {
-            cout<<"missing parenthesis."<<endl;
-        }
-
-
-        // Integer detection
-        for(int i=0;i<line.length();i++)
-        {
-            if(isdigit(line[i]))
-            {
-                string num="";
-
-                while(i<line.length() && isdigit(line[i]))
-                {
-                    num += line[i];
-                    i++;
-                }
-
-                cout<<"'"<<num<<"' is an integer"<<endl;
-            }
-        }
-
-
-
-        // Invalid variable like 1f
-        for(int i=0;i<line.length()-1;i++)
-        {
-            if(isdigit(line[i]) && isalpha(line[i+1]))
-            {
-                string invalid="";
-
-                while(i<line.length() &&
-                     (isalnum(line[i])))
-                {
-                    invalid+=line[i];
-                    i++;
-                }
-
-                cout<<"Invalid variable - "<<invalid<<endl;
-            }
-        }
-
-
-
-        // Invalid expression
-        if(line.find("@")!=-1)
-        {
-            cout<<"Invalid expression in line - "
-                <<lineNo<<endl;
-        }
-
-        if(line.find("-;")!=-1)
-        {
-            cout<<"Invalid expression in line - "
-                <<lineNo<<endl;
-        }
-
-
-
-        // Variable detection
-        for(int i=0;i<line.length();i++)
-        {
-            if(isalpha(line[i]) || line[i]=='_')
-            {
-                string word="";
-
-                while(i<line.length() &&
-                     (isalnum(line[i]) || line[i]=='_'))
-                {
-                    word+=line[i];
-                    i++;
-                }
-
-
-                if(word!="include" &&
-                   word!="int" &&
-                   word!="string" &&
-                   word!="using" &&
-                   word!="namespace" &&
-                   word!="return")
-                {
-                    bool exists=false;
-
-                    for(int j=0;j<varCount;j++)
-                    {
-                        if(variables[j]==word)
-                            exists=true;
-                    }
-
-                    if(!exists)
-                    {
-                        variables[varCount]=word;
-                        varCount++;
-                    }
-                }
-            }
-        }
-
-
-
-        // Operator detection
-        for(int i=0;i<line.length();i++)
-        {
-            if(line[i]=='+' || line[i]=='-' || line[i]=='*')
-            {
-                bool exists=false;
-
-                for(int j=0;j<opCount;j++)
-                {
-                    if(operators[j]==line[i])
-                        exists=true;
-                }
-
-                if(!exists)
-                {
-                    operators[opCount]=line[i];
-                    opCount++;
-                }
-            }
-        }
+      if(str[i]=='/' && str[i+1]=='/')
+       {
+         printf("Comment in line - %d\n", lineNumber);
+         return;
+       }
 
     }
 
 
-cout<<"   ----    "<<endl;
-    // Print variables
-    for(int i=0;i<varCount;i++)
-    {
-        cout<<"Variable-"<<i+1<<": "
-            <<variables[i]<<endl;
-    }
 
-    cout<<"Total number of the variables: "
-        <<varCount<<endl;
+	// Check for header file
+	if (len > 0 && str[0] == '#') {
+		// Extract library name
+		for (int i = 0; i < len; i++) {
+			if (str[i] == '<') {
+				int start = i + 1;
+				int end = start;
+				while (end < len && str[end] != '>') end++;
+				if (end < len) {
+					char* lib = subString(str, start, end - 1);
+					addUniqueLibrary(lib);
+					free(lib);
+				}
+				break;
+			}
+		}
+		return;
+	}
+	  for(int i=0; i<len; i++)
+      {
+        if(str[i] == '"')
+         {
+           str[i] = ' ';
+           i++;
 
+           while(i < len && str[i] != '"')
+           {
+             str[i] = ' ';
+             i++;
+           }
 
+           if(i < len)
+            str[i] = ' ';
+        }
+      }
 
-    // Print operators
-    for(int i=0;i<opCount;i++)
-    {
-        cout<<"Operator-"<<i+1<<": "
-            <<operators[i]<<endl;
-    }
+	// Tokenize the line
+	while (right <= len && left <= right) {
+		if (isDelimiter(str[right]) == false) {
+			right++;
+		}
+		if (isDelimiter(str[right]) == true && left == right) {
+			if (isOperator(str[right]) == true) {
+				char op[2] = {str[right], '\0'};
+				tokens[tokenCount] = (char*)malloc(2);
+				strcpy(tokens[tokenCount], op);
+				tokenCount++;
+				addUniqueOperator(str[right]);
+			}
+			right++;
+			left = right;
+		} else if (isDelimiter(str[right]) == true && left != right || (right == len && left != right)) {
+			char* subStr = subString(str, left, right - 1);
 
-    cout<<"Total number of the operators: "
-        <<opCount<<endl;
+			if (isKeyword(subStr) == false) {
+				if (isInteger(subStr) == true) {
+					printf("'%s' is an integer\n", subStr);
+					tokens[tokenCount] = (char*)malloc(strlen(subStr) + 1);
+					strcpy(tokens[tokenCount], subStr);
+					tokenCount++;
+				} else if (isRealNumber(subStr) == true) {
+					printf("'%s' is a real number\n", subStr);
+					tokens[tokenCount] = (char*)malloc(strlen(subStr) + 1);
+					strcpy(tokens[tokenCount], subStr);
+					tokenCount++;
+				} else if (validIdentifier(subStr) == true) {
+					addUniqueVariable(subStr);
+					tokens[tokenCount] = (char*)malloc(strlen(subStr) + 1);
+					strcpy(tokens[tokenCount], subStr);
+					tokenCount++;
+				} else {
+					addInvalidVariable(subStr);
+					tokens[tokenCount] = (char*)malloc(strlen(subStr) + 1);
+					strcpy(tokens[tokenCount], subStr);
+					tokenCount++;
+				}
+			} else {
+				tokens[tokenCount] = (char*)malloc(strlen(subStr) + 1);
+				strcpy(tokens[tokenCount], subStr);
+				tokenCount++;
+			}
+			free(subStr);
+			left = right;
+		}
+	}
 
+	// Check expression validity
+	if (tokenCount >= 3) {
+		bool valid = checkValidExpression(tokens, tokenCount);
+		if (!valid && str[0] != '#' && str[0] != '/') {
+			// Check if it's not a variable declaration line
+			bool isDeclaration = false;
+			for (int i = 0; i < tokenCount; i++) {
+				if (isKeyword(tokens[i])) {
+					isDeclaration = true;
+					break;
+				}
+			}
+			if (!isDeclaration) {
+				printf("Invalid expression in line - %d\n", lineNumber);
+			}
+		}
+	}
 
-    fin.close();
+	// Clean up tokens
+	for (int i = 0; i < tokenCount; i++) {
+		free(tokens[i]);
+	}
+	return;
+}
+// DRIVER FUNCTION
+int main(){
+	ifstream MyReadFile("program.cpp");
+	string line;
 
-    return 0;
+	while(getline(MyReadFile, line)) {
+		lineNumber++;
+		int n = line.length();
+		char str[n + 1];
+		strcpy(str, line.c_str());
+		parse(str);
+	}
+	MyReadFile.close();
+
+	// Print all libraries
+	printf("\n");
+	for (int i = 0; i < libCount; i++) {
+		printf("Library- %d: %s\n", i+1, libArray[i]);
+	}
+
+	// Print all variables
+	printf("\n");
+	for (int i = 0; i < idCount; i++) {
+		printf("Variable- %d: %s\n", i+1, idArray[i]);
+	}
+	printf("Total number of the variables: %d\n", idCount);
+
+	// Print all operators
+	printf("\n");
+	for (int i = 0; i < opCount; i++) {
+		printf("Operator- %d: %s\n", i+1, opArray[i]);
+	}
+	printf("Total number of the operators: %d\n", opCount);
+
+	// Print invalid variables
+	if (invalidVarCount > 0) {
+		printf("\n");
+		for (int i = 0; i < invalidVarCount; i++) {
+			printf("Invalid variable - %s\n", invalidVarArray[i]);
+		}
+	}
+
+	if (semicolonMissing) {
+		printf("Errors!\n");
+	}
+
+	// Clean up
+	for (int i = 0; i < idCount; i++) free(idArray[i]);
+	for (int i = 0; i < opCount; i++) free(opArray[i]);
+	for (int i = 0; i < libCount; i++) free(libArray[i]);
+	for (int i = 0; i < invalidVarCount; i++) free(invalidVarArray[i]);
+
+	return (0);
 }
